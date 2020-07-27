2896f1d77f461042542a0a1894801b35b1e91e4f
carintimber <carintimber@sjtu.edu.cn>
2020-07-27 10:47:52 +0800

    HW0

---
+++ HW0JINZITA.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class HW0JINZITA {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+         for (int i = 1; i < 6; i++){
+            for (int j = 1; j < i + 1; j++){
+                System.out.print("*");
+        }
+            System.out.print("\n");
+    }
+    }
+}
